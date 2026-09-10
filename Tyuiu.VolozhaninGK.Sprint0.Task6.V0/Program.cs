0d19829065d0997ebded4fec16072d6bc346a5b4
grio555567-hub <grio555567@gmail.com>
2026-09-10 17:44:09 +0500

    Task6

---
+++ Tyuiu.VolozhaninGK.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Tyuiu.VolozhaninGK.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.VolozhaninGK.Sprint0.Task6.V0
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
