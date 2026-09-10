94629f270c0e0fbe29b5d007de02a6a739aa0639
grio555567-hub <grio555567@gmail.com>
2026-09-10 16:43:46 +0500

    Task3

---
+++ Tyuiu.VolozhaninGK.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.VolozhaninGK.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ Tyuiu.VolozhaninGK.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Tyuiu.VolozhaninGK.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.VolozhaninGK.Sprint0.Task3.V0
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
