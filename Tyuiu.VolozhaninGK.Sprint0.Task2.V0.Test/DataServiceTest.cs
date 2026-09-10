5cf9ce1a1c7565ceec813aee0c164238c0462294
grio555567-hub <grio555567@gmail.com>
2026-09-10 16:24:09 +0500

    Task2

---
+++ Tyuiu.VolozhaninGK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+using Tyuiu.VolozhaninGK.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.VolozhaninGK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Григорий";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Григорий", res);
+        }
+    }
+}
---
+++ Tyuiu.VolozhaninGK.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Tyuiu.VolozhaninGK.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.VolozhaninGK.Sprint0.Task2.V0
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Григорий"));
+            Console.ReadKey();
+        }
+    }
+}
