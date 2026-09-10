e3c1078197352499f524b599584ede2b83447c7a
grio555567-hub <grio555567@gmail.com>
2026-09-10 17:07:39 +0500

    Task4

---
+++ Tyuiu.VolozhaninGK.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using Tyuiu.VolozhaninGK.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.VolozhaninGK.Sprint0.Task3.V0
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Substraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+            Console.ReadKey();
+        }
+    }
+}
